f2bcf4c2e8ddd35927ab157607ed9cbbfd21eb2f
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2023-12-27 18:26:10 +0800

    fix: unit text coverage

--- src/utils/string.test.ts
+++ src/utils/string.test.ts
@@ -1,53 +1,72 @@
-import { describe, it, expect } from '@jest/globals';
-
 import { trimText, trimAddress, trimTextByHeadTail } from './string';
 
 describe('trimText', () => {
-  it('trim English text when text length exceed `100`', async () => {
+  it('trim text with default length `100` when parameter length is not provided', async () => {
     const content =
       'Lorem ipsum, train, feed, love and evolve your very own mischievous little pet. Modernipsum dolor sit amet scuola romana neo-fauvism idealism carolingian, rococo de stijl mail art avant-garde conceptual art relational art. Lorem ipsum, train, feed, love and evolve your very own mischievous little pet. Modernipsum dolor sit amet scuola romana neo-fauvism idealism carolingian, rococo de stijl mail art avant-garde conceptual art relational art.';
 
-    expect(trimText(content, 100)).toBe(`${content.slice(0, 100)}...`);
+    expect(trimText(content)).toBe(`${content.slice(0, 100)}...`);
   });
 
-  it('does not trim text when ltext ength shorter than `100`', async () => {
+  it('trim text with with the provided length', async () => {
+    const content =
+      'Lorem ipsum, train, feed, love and evolve your very own mischievous little pet. Modernipsum dolor sit amet scuola romana neo-fauvism idealism carolingian, rococo de stijl mail art avant-garde conceptual art relational art. Lorem ipsum, train, feed, love and evolve your very own mischievous little pet. Modernipsum dolor sit amet scuola romana neo-fauvism idealism carolingian, rococo de stijl mail art avant-garde conceptual art relational art.';
+
+    expect(trimText(content, 50)).toBe(`${content.slice(0, 50)}...`);
+  });
+
+  it('does not trim text when provided text is empty', async () => {
+    const content = '';
+
+    expect(trimText(content, 50)).toStrictEqual(content);
+  });
+
+  it('does not trim text when text length shorter than given length', async () => {
     const content = 'Lorem ipsum';
-    expect(trimText(content, 100)).toStrictEqual(content);
+
+    expect(trimText(content, 50)).toStrictEqual(content);
   });
 
-  it('trim text with leading space and tailing space', async () => {
+  it('trim leading space and tailing space', async () => {
     const content = ' Lorem ipsum ';
-    expect(trimText(content, 100)).toBe('Lorem ipsum');
+
+    expect(trimText(content, 50)).toBe('Lorem ipsum');
   });
 
-  it('trim German text when text length exceed `100`', async () => {
+  it('trim German text', async () => {
     const content =
       'Man erzählt von dem ich mich an die Gurgel faßt wie ein Meuchelmörder, dann mein Herz näher angeht. Man erzählt von dem ich mich an die Gurgel faßt wie ein Meuchelmörder, dann mein Herz näher angeht. Man erzählt von dem ich mich an die Gurgel faßt wie ein Meuchelmörder, dann mein Herz näher angeht. Man erzählt von dem ich mich an die Gurgel faßt wie ein Meuchelmörder, dann mein Herz näher angeht. Man erzählt von dem ich mich an die Gurgel faßt wie ein Meuchelmörder, dann mein Herz näher angeht.';
 
-    expect(trimText(content, 100)).toBe(
-      `${[...content].slice(0, 100).join('')}...`,
-    );
+    expect(trimText(content)).toBe(`${[...content].slice(0, 100).join('')}...`);
   });
 });
 
 describe('trimTextByHeadTail', () => {
-  it('trim English text when text length exceed `4`', async () => {
+  it('trim text when text length exceed the sum of parameters `heading` and `tailing`', async () => {
     const content = 'aaaaa';
 
     expect(trimTextByHeadTail(content, 2, 2)).toBe('aa...aa');
   });
 
-  it('does not trim text when text length shorter than `4`', async () => {
+  it('does not trim text when text length is not exceed the sum of parameters `heading` and `tailing`', async () => {
     const content = 'aa';
+
     expect(trimTextByHeadTail(content, 2, 2)).toBe('aa');
   });
 
-  it('trim text with leading space and tailing space', async () => {
+  it('does not trim text when provided text is empty', async () => {
+    const content = '';
+
+    expect(trimTextByHeadTail(content, 5, 5)).toStrictEqual(content);
+  });
+
+  it('trim leading space and tailing space', async () => {
     const content = ' aa ';
+
     expect(trimTextByHeadTail(content, 2, 2)).toBe('aa');
   });
 
-  it('trim German text when text length exceed `4`', async () => {
+  it('trim German text', async () => {
     const content = 'äääää';
 
     expect(trimTextByHeadTail(content, 2, 2)).toBe(`ää...ää`);
@@ -55,7 +74,7 @@ describe('trimTextByHeadTail', () => {
 });
 
 describe('trimAddress', () => {
-  it('trim address when address length exceed limit', async () => {
+  it('trim address', async () => {
     const address = '0x6B24aE0ABbeb67058D07b891aF415f288eA57Cc7';
 
     expect(trimAddress(address)).toBe('0x6B24a...57Cc7');
