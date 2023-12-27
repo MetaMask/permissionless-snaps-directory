67b900e047fff9e11405fdd980ca8528fc878e62
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2023-12-27 18:10:18 +0800

    fix: unit text coverage

--- src/components/Rating.test.tsx
+++ src/components/Rating.test.tsx
@@ -9,7 +9,18 @@ describe('Rating', () => {
     expect(queryByTestId('empty-0')).toBeInTheDocument();
   });
 
-  it('renders correctly when provided parameter `rate` larger than `maxRate`', async () => {
+  it('renders with default `maxRate` when `maxRate` is not provided', async () => {
+    const { queryByTestId } = render(<Rating rate={1} />);
+
+    expect(queryByTestId('filled-0')).toBeInTheDocument();
+    expect(queryByTestId('empty-0')).toBeInTheDocument();
+    expect(queryByTestId('empty-1')).toBeInTheDocument();
+    expect(queryByTestId('empty-2')).toBeInTheDocument();
+    expect(queryByTestId('empty-3')).toBeInTheDocument();
+    expect(queryByTestId('empty-4')).not.toBeInTheDocument();
+  });
+
+  it('renders correctly when provided `rate` larger than `maxRate`', async () => {
     const { queryByTestId } = render(<Rating rate={10} maxRate={2} />);
 
     expect(queryByTestId('filled-0')).toBeInTheDocument();
