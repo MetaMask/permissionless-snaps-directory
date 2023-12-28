ea5a333d5b402853558c7b87bca14983d7e1dd11
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2023-12-28 18:39:33 +0800

    feat: Account profile tabs

---
+++ src/features/account/components/index.ts
@@ -0,0 +1,4 @@
+export * from './security-reports/SecurityReportsTabPanel';
+export * from './reviews/ReviewsTabPanel';
+export * from './trusted-circle/TrustedCircleTabPanel';
+export * from './snaps/MySnapsTabPanel';
