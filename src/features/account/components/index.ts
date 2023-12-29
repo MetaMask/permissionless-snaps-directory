adb170c6c1c01c9b3c4fed6bb592ae8dded94f3b
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2023-12-29 13:54:57 +0800

    fix: add export account info

--- src/features/account/components/index.ts
+++ src/features/account/components/index.ts
@@ -2,3 +2,4 @@ export * from './security-reports/SecurityReportsTabPanel';
 export * from './reviews/ReviewsTabPanel';
 export * from './trusted-circle/TrustedCircleTabPanel';
 export * from './snaps/MySnapsTabPanel';
+export * from './AccountInfo';
