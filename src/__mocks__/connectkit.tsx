10e292f50e4e359580b206db555a92082c147b49
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-01-20 16:48:15 +0800

    fix: add back mock connect kit for unit test

--- src/__mocks__/connectkit.tsx
+++ src/__mocks__/connectkit.tsx
@@ -10,4 +10,32 @@ export const MockConnectKitProvider: FunctionComponent<
   return <div className="mock-connectkit-provider">{children}</div>;
 };
 
+export type MockConnectKitButtonCustomProps = {
+  children: (props: {
+    isConnected: boolean;
+    show: () => void;
+    truncatedAddress: string;
+    ensName: string | null;
+  }) => ReactNode;
+};
+
+export const MockConnectKitButtonCustom: FunctionComponent<
+  MockConnectKitButtonCustomProps
+> = ({ children }) => {
+  return (
+    <div className="mock-connectkit-button-custom">
+      {children({
+        isConnected: false,
+        show: jest.fn(),
+        truncatedAddress: '0x...',
+        ensName: 'mock.ens.name',
+      })}
+    </div>
+  );
+};
+
+export const ConnectKitButton = {
+  Custom: MockConnectKitButtonCustom,
+};
+
 export const ConnectKitProvider = MockConnectKitProvider;
