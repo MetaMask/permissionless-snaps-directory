fda8dd7e1292cf03521b9c90a9107d8c9680771c
alainncls <alainncls@users.noreply.github.com>
2024-02-23 16:45:33 +0000

    deploy: feed01fec14472a2c21f48db663c13cf123c0279

---
+++ pr/120/component---src-pages-community-tsx-1f63d8edbe5f90e5fda8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_metamask_snaps_directory=self.webpackChunk_metamask_snaps_directory||[]).push([[619],{2530:function(e,s,a){a.r(s);var n=a(22338),t=a(67294),c=a(48352),r=a(96005),o=a(16673),i=a(85893);s.default=()=>{const e=(0,o.I0)();return(0,t.useEffect)((()=>{e((0,r.J)()).catch((e=>console.log(e)))}),[e]),(0,i.jsx)(n.W,{maxWidth:"container.xl",paddingTop:"0",marginTop:20,children:(0,i.jsx)(c.TW,{})})}}}]);
+//# sourceMappingURL=component---src-pages-community-tsx-1f63d8edbe5f90e5fda8.js.map
