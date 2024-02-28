75db0a5278cb991dc0647d1bc43e4fe5eac849df
stanleyyconsensys <stanleyyconsensys@users.noreply.github.com>
2024-02-28 09:08:59 +0000

    deploy: cb3d05c13ef6dcbbb7d556d3bccf495f54ed6398

--- pr/133/component---src-pages-community-tsx-c1f9d63d93731e98b3dc.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_metamask_snaps_directory=self.webpackChunk_metamask_snaps_directory||[]).push([[619],{2530:function(e,s,a){a.r(s);var n=a(22338),t=a(67294),c=a(77009),r=a(96005),o=a(16673),i=a(85893);s.default=()=>{const e=(0,o.I0)();return(0,t.useEffect)((()=>{e((0,r.J)()).catch((e=>console.log(e)))}),[e]),(0,i.jsx)(n.W,{maxWidth:"container.xl",paddingTop:"0",marginTop:20,children:(0,i.jsx)(c.TW,{})})}}}]);
-//# sourceMappingURL=component---src-pages-community-tsx-c1f9d63d93731e98b3dc.js.map
