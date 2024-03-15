2fc9bebb8164a4f3409893ee29e0ebc34f38c5da
Satyajeet Kolhapure <satyajeet.kolhapure@consensys.net>
2024-03-15 14:29:45 +0000

    added comments on connections store

--- src/features/account/connections/store.ts
+++ src/features/account/connections/store.ts
@@ -23,12 +23,13 @@ const getConnectedNodesForLevel = (
   accountAssertions: AccountAssertionsState,
   accountId: string,
   level: number,
-  igonreAccountIds: string[] = [],
+  ignoreAccountIds: string[] = [],
 ) => {
   const accountConnections: AccountConnections = {
     nodes: [],
     links: [],
   };
+  // As level 1 is called from outside, the main account is added.
   if (level === 1) {
     accountConnections.nodes.push({
       id: accountId,
@@ -36,12 +37,18 @@ const getConnectedNodesForLevel = (
       isMain: level === 1,
     });
   }
+
+  // Find assertions for the account ignoring the previous ones
   const endorsementsOnAccount = accountAssertions.accountAssertions.filter(
     (assertion) =>
       assertion.accountId === accountId &&
-      !igonreAccountIds.includes(assertion.issuer.toLowerCase()),
+      !ignoreAccountIds.includes(assertion.issuer.toLowerCase()),
   );
+
+  // Get top 2 connections for the account
   const topTwoConnections = endorsementsOnAccount.slice(0, 2);
+
+  // Adding the top two connections to the final result
   topTwoConnections.forEach((connection) => {
     accountConnections.links.push({
       source: connection.accountId,
@@ -53,17 +60,23 @@ const getConnectedNodesForLevel = (
       isMain: false,
     });
   });
+
   if (level < 2) {
+    // Increase the level
     const connectedNodeLevel = level + 1;
-    igonreAccountIds.push(
+
+    // Add current accounts to ignored list
+    ignoreAccountIds.push(
       ...accountConnections.nodes.map((node) => node.id.toLowerCase()),
     );
+
+    // For each level the top two connections are added to final result
     topTwoConnections.forEach((connection) => {
       const issuerConnections = getConnectedNodesForLevel(
         accountAssertions,
         connection.issuer,
         connectedNodeLevel,
-        igonreAccountIds,
+        ignoreAccountIds,
       );
       accountConnections.links.push(...issuerConnections.links);
       accountConnections.nodes.push(...issuerConnections.nodes);
