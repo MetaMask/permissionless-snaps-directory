32e7ea77d49de7c97274d6d5399512551e1d6ffe
Alain Nicolas <alainncls@users.noreply.github.com>
2024-04-26 14:29:08 +0200

    fix: DID is displayed instead of ENS/address in "Auditor" for a Snap (#193)

--- src/features/snap/components/MetadataAuditItem.test.tsx
+++ src/features/snap/components/MetadataAuditItem.test.tsx
@@ -1,17 +1,9 @@
 import { MetadataAuditItem } from './MetadataAuditItem';
-import { useVerifiableCredential } from '../../../hooks';
-import { VALID_ACCOUNT_1, render } from '../../../utils/test-utils';
+import { render, VALID_ACCOUNT_1 } from '../../../utils/test-utils';
 
 jest.mock('wagmi', () => ({
   ...jest.requireActual('wagmi'),
   useEnsName: jest.fn(),
-}));
-
-jest.mock('../../../hooks');
-
-jest.mock('../../../hooks/useVerifiableCredential', () => ({
-  ...jest.requireActual('../../../hooks/useVerifiableCredential'),
-  useVerifiableCredential: jest.fn(),
 }));
 
 jest.mock('../../../components/EntityName', () => ({
@@ -19,17 +11,6 @@ jest.mock('../../../components/EntityName', () => ({
 }));
 
 describe('MetadataAuditItem', () => {
-  let mockUseVerifiableCredential: jest.Mock;
-  beforeEach(() => {
-    mockUseVerifiableCredential = useVerifiableCredential as jest.Mock;
-    mockUseVerifiableCredential.mockClear();
-    mockUseVerifiableCredential.mockReturnValue({
-      accountVCBuilder: {
-        getSubjectDid: jest.fn().mockReturnValue(VALID_ACCOUNT_1),
-      },
-    });
-  });
-
   it('renders the auditor entity', async () => {
     const { queryByText, queryByTestId } = render(
       <MetadataAuditItem auditorAddresses={[VALID_ACCOUNT_1]} />,
