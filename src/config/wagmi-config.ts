89cd0f67da8aca8527cd204b8107a79ed92b9071
Alain Nicolas <alainncls@users.noreply.github.com>
2024-05-28 15:51:30 +0200

    feat: AAU, I want to connect via WalletConnect (#217)

--- src/config/wagmi-config.ts
+++ src/config/wagmi-config.ts
@@ -2,12 +2,24 @@ import { configureChains } from '@wagmi/core';
 import { mainnet } from '@wagmi/core/chains';
 import { createConfig } from 'wagmi';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient } = configureChains([mainnet], [publicProvider()]);
 
+const walletConnectProjectId = 'f9f08df5485d8e71127deb33ee1be4f7';
+
 export const WAGMI_CONFIG = createConfig({
   autoConnect: true,
-  connectors: [new MetaMaskConnector({ chains })],
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: walletConnectProjectId,
+        showQrModal: false,
+      },
+    }),
+  ],
   publicClient,
 });
