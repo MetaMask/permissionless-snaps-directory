8e1a6640b9e330bbed9da1b6135c75d95f3b96f3
Satyajeet Kolhapure <77279246+satyajeetkolhapure@users.noreply.github.com>
2024-05-28 14:41:24 +0100

    feat: AAU, I want to see the list of users who have endorsed or reported a snap or a user (#218)

--- src/features/account/components/technical-expertise/TechnicalExpertiseItem.tsx
+++ src/features/account/components/technical-expertise/TechnicalExpertiseItem.tsx
@@ -1,9 +1,11 @@
-import { HStack, Text, VStack } from '@chakra-ui/react';
+import { Button, HStack, Text, VStack, useDisclosure } from '@chakra-ui/react';
 import { t } from '@lingui/macro';
 import type { Address } from '@wagmi/core';
 import type { FunctionComponent } from 'react';
 
+import { IssuersListModal } from '../../../../components';
 import { EntityName } from '../../../../components/EntityName';
+import { Value } from '../../assertions/enums';
 import type { AccountAssertionState } from '../../assertions/store';
 import type { TrustworthinessScope } from '../../assertions/types';
 
@@ -16,6 +18,7 @@ export type TechnicalExpertiseItemProps = {
 export const TechnicalExpertiseItem: FunctionComponent<
   TechnicalExpertiseItemProps
 > = ({ endorsements, type, myAddress }) => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   const isEndorsedByMe = endorsements.find(
     (endorsement) =>
       endorsement.issuerId.toLowerCase() === myAddress?.toLowerCase(),
@@ -27,38 +30,62 @@ export const TechnicalExpertiseItem: FunctionComponent<
     ? maxEndorsementsToDisplay - 1
     : maxEndorsementsToDisplay;
 
+  const subject = endorsements[0]?.subjectId ?? '';
+
+  const issuers = endorsements.map((endorsement) => ({
+    address: endorsement.issuerId,
+    issuanceDate: endorsement.issuanceDate,
+  }));
+
   return (
-    <VStack mb={2} alignItems={'start'}>
-      <Text fontWeight={'medium'}>{type}</Text>
-      <HStack>
-        <Text>{t`Endorsed by`}</Text>
-        {isEndorsedByMe && myAddress && (
-          <>
-            <EntityName
-              key={`${myAddress}-${type}`}
-              subject={myAddress.toLowerCase()}
-              isSnap={false}
-              title={`${t`you`}`}
-            />
-            {endorsementsCount > 1 && <Text ml={-1}>,</Text>}
-          </>
-        )}
-        {endorsements.slice(0, maxEndorsements).map((endorsement, index) => (
-          <>
-            <EntityName
-              key={`${endorsement.issuerId}-${index}`}
-              subject={endorsement.issuerId}
-              isSnap={false}
-            />
-            {index < maxEndorsements - 1 && <Text ml={-1}>,</Text>}
-          </>
-        ))}
-        {endorsementsCount > maxEndorsements + 1 && (
-          <Text ml={0}>{`+ ${
-            endorsementsCount - maxEndorsementsToDisplay
-          } ${t`more`}`}</Text>
-        )}
-      </HStack>
-    </VStack>
+    <>
+      <VStack mb={2} alignItems={'start'}>
+        <Text fontWeight={'medium'}>{type}</Text>
+        <HStack>
+          <Text>{t`Endorsed by`}</Text>
+          {isEndorsedByMe && myAddress && (
+            <>
+              <EntityName
+                key={`${myAddress}-${type}`}
+                subject={myAddress.toLowerCase()}
+                isSnap={false}
+                title={`${t`you`}`}
+              />
+              {endorsementsCount > 1 && <Text ml={-1}>,</Text>}
+            </>
+          )}
+          {endorsements.slice(0, maxEndorsements).map((endorsement, index) => (
+            <>
+              <EntityName
+                key={`${endorsement.issuerId}-${index}`}
+                subject={endorsement.issuerId}
+                isSnap={false}
+              />
+              {index < maxEndorsements - 1 && <Text ml={-1}>,</Text>}
+            </>
+          ))}
+          {endorsementsCount > maxEndorsements + 1 && (
+            <Button onClick={onOpen} variant="link" fontWeight="regular">
+              <Text variant="blue">
+                {`+ ${endorsementsCount - maxEndorsementsToDisplay} ${t`more`}`}
+              </Text>
+            </Button>
+          )}
+        </HStack>
+      </VStack>
+      {isOpen && (
+        <IssuersListModal
+          subject={subject}
+          isSnap={false}
+          assertionType={Value.Endorsement}
+          reason={type}
+          issuers={issuers}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          {''}
+        </IssuersListModal>
+      )}
+    </>
   );
 };
